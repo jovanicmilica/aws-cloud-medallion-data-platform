25e8616c586a0b77d4a8e3cce23c79c2886cbf5d
Milica Jovanic <jovanic.sv9.2023@uns.ac.rs>
2026-05-15 20:13:11 +0200

    add initial springboot project

---
+++ src/test/java/com/medallion/social_media_data_lake/SocialMediaDataLakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medallion.social_media_data_lake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaDataLakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
